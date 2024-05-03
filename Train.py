13d1411398bc9c3954e4c53087d1e4606ff1d606
Alireza Abdolhosseini Ghouchani <146181928+Alireza-Abdolhosseini@users.noreply.github.com>
2024-05-03 11:49:34 +0330

    Update 1.1

--- Train.py
+++ Train.py
@@ -3,7 +3,7 @@ print("Loading Libraries ...")
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-import torchvision.datasets as dsets
+from torchvision.datasets import MNIST
 from torch import max as tmax
 from torch import save as tsave
 from torch.utils.data import DataLoader
@@ -11,15 +11,16 @@ from Neural_Network import Net
 
 # Loading Dataset
 print("Loading MNIST Dataset ...")
-transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor(),
-                                     transforms.Lambda(lambda x: transforms.functional.invert(x))])
+transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
+                                transforms.ToTensor(),
+                                transforms.Lambda(lambda x: transforms.functional.invert(x))])
 
-train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=transform)
-validation_dataset = dsets.MNIST(root='./data', train=False, download=True, transform=transform)
+train_dataset = MNIST(root='./data', train=True, download=True, transform=transform)
+validation_dataset = MNIST(root='./data', train=False, download=True, transform=transform)
 
 
 # Defining the dimension of input and output of the Neural Network
-input_dim = validation_dataset[0][0].shape[1] ** 2 # 28 * 28
+input_dim = validation_dataset[0][0].shape[1] ** 2 # 28 * 28 = 784
 outputs = list()
 for i in range(len(validation_dataset)):
     outputs.append(validation_dataset[i][1])
@@ -35,17 +36,18 @@ criterion = nn.CrossEntropyLoss()
 train_loader = DataLoader(dataset=train_dataset, batch_size=500, shuffle=True)
 validation_loader = DataLoader(dataset=validation_dataset, batch_size=2000, shuffle=False)
 
-layers = [input_dim, 300, output_dim]
+layers = [input_dim, 400, output_dim]
 model = Net(layers)
 
 optimizer = optim.SGD(model.parameters(), lr=0.5, momentum=0.9)
 
-scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.5, total_iters=30)
+scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=0.02, total_iters=5)
 
 # Start training
 print("Training ...")
 
 epochs = 20
+
 for epoch in range(epochs):
     for x_train, y_train in train_loader:
         optimizer.zero_grad()
@@ -53,6 +55,9 @@ for epoch in range(epochs):
         loss = criterion(z, y_train)
         loss.backward()
         optimizer.step()
+
+    if epoch >= 10:
+        scheduler.step()
 
     correct = 0
     for x_test, y_test in validation_loader:
@@ -66,4 +71,4 @@ for epoch in range(epochs):
 
 # Saving results
 print("Saving Model ...")
-tsave(model.state_dict(), "model.pt")
+tsave(model.state_dict(), f"model.pt")
