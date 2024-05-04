a9e146546e7919bede517fe1e87973b115b2d996
Alireza Abdolhosseini Ghouchani <146181928+Alireza-Abdolhosseini@users.noreply.github.com>
2024-05-04 12:56:04 +0330

    Update 1.2

--- Neural_Network.py
+++ Neural_Network.py
@@ -8,17 +8,43 @@ class Net(nn.Module):
         self.dropout = nn.Dropout(p=0.1)
         self.hidden = nn.ModuleList()
         self.cnn = nn.ModuleList()
+        self.bn = nn.ModuleList()
         self.maxpooling = nn.ModuleList()
 
         self.cnn.append(nn.Conv2d(in_channels=1, # Gray pictures
                                         out_channels=32,
-                                        kernel_size=2,
-                                        stride=2,
+                                        kernel_size=5,
+                                        stride=1,
                                         padding=0))
+
 
         self.maxpooling.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
-        layers[0] = 1568 # Number of nodes after convolution and maxpooling
+        self.bn.append(nn.BatchNorm2d(32))
+
+        self.cnn.append(nn.Conv2d(in_channels=32,
+                                        out_channels=64,
+                                        kernel_size=3,
+                                        stride=1,
+                                        padding=0))
+
+
+        self.maxpooling.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.bn.append(nn.BatchNorm2d(64))
+
+        self.cnn.append(nn.Conv2d(in_channels=64,
+                                        out_channels=128,
+                                        kernel_size=3,
+                                        stride=1,
+                                        padding=0))
+
+
+        self.maxpooling.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.bn.append(nn.BatchNorm2d(128))
+
+        layers[0] = 128 # Number of nodes after convolution and maxpooling
 
         for input, output in zip(layers, layers[1:]):
             layer = nn.Linear(input, output)
@@ -33,6 +59,7 @@ class Net(nn.Module):
         for i, layer in zip(range(l), self.cnn):
             x = relu(layer(x))
             x = self.maxpooling[i](x)
+            x = self.bn[i](x)
 
         x = x.view(x.size(0), -1)
         l = len(self.hidden)
@@ -41,7 +68,7 @@ class Net(nn.Module):
                 x = self.dropout(x)
                 x = relu(layer(x))
             else:
-                x = nn.functional.softmax(layer(x), dim=1)
+                x = nn.functional.log_softmax(layer(x), dim=1)
 
         return x
 
