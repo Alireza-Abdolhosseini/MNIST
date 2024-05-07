8a976a8de40ca6acb976989aae0d703d8d435a7f
Alireza Abdolhosseini Ghouchani <146181928+Alireza-Abdolhosseini@users.noreply.github.com>
2024-05-07 21:57:07 +0330

    Update 1.3

--- Train.py
+++ Train.py
@@ -1,4 +1,4 @@
-# update 1.2
+# update 1.3
 
 print("Loading Libraries ...")
 
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 from torchvision.datasets import MNIST
+import torch.nn.functional as F
 from torch import max as tmax
 from torch import save as tsave
 from torch.utils.data import DataLoader
@@ -35,8 +36,8 @@ output_dim = len(outputs) # 0 -> 9 = 10
 print("Building Neural Network ...")
 criterion = nn.CrossEntropyLoss()
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=500, shuffle=True)
-validation_loader = DataLoader(dataset=validation_dataset, batch_size=2000, shuffle=False)
+train_loader = DataLoader(dataset=train_dataset, batch_size=1000, shuffle=True)
+validation_loader = DataLoader(dataset=validation_dataset, batch_size=5000, shuffle=False)
 
 layers = [input_dim, 400, output_dim]
 model = Net(layers)
@@ -48,29 +49,40 @@ scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=
 # Start training
 print("Training ...")
 
-epochs = 20
+epochs = 40
 
 for epoch in range(epochs):
+    i = 0
     for x_train, y_train in train_loader:
         optimizer.zero_grad()
         z = model(x_train)
         loss = criterion(z, y_train)
         loss.backward()
         optimizer.step()
+        i += 1
+        print("  Training Epoch {} ----- {:.2f} %".format(epoch + 1, float(i / len(train_loader) * 100)), end="\r")
+
+
+    i = 0
+    correct = 0
+    validation_loss = 0
+    for x_test, y_test in validation_loader:
+        z = model(x_test)
+        _, label = tmax(z, 1)
+        validation_loss += F.nll_loss(z, y_test, size_average=False).data.item()
+        correct += (label == y_test).sum().item()
+        i += 1
+        print("  Validation Epoch {} ----- {:.2f} %".format(epoch + 1, float(i / len(validation_loader) * 100)), end="\r")
+
+    validation_loss /= len(validation_dataset)
+    accuracy = 100 * (correct / len(validation_dataset))
 
     if epoch >= 10:
         scheduler.step()
 
-    correct = 0
-    for x_test, y_test in validation_loader:
-        z = model(x_test)
-        _, label = tmax(z, 1)
-        correct += (label == y_test).sum().item()
-
-    accuracy = 100 * (correct / len(validation_dataset))
-    print(f"Epoch {epoch + 1}/{epochs} -------------- Accuracy = {round(accuracy, 2)} %")
+    print(f"Epoch {epoch + 1}/{epochs} -------------- Accuracy = {round(accuracy, 2)} %  Loss = {round(validation_loss, 4)}")
+    tsave(model.state_dict(), f"model.pt")
 
 
 # Saving results
 print("Saving Model ...")
-tsave(model.state_dict(), f"model.pt")
